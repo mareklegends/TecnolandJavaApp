7da7a6b4cd1f8e852a1f9b305730753dfa9e19f5
alumno <alumno@aula1-02>
2018-11-23 17:26:41 +0100

    casi acabada 2.1

--- TecnolandJavaApp/src/tecnolandjavaapp/TecnolandJavaApp.java
+++ TecnolandJavaApp/src/tecnolandjavaapp/TecnolandJavaApp.java
@@ -22,7 +22,9 @@ public class TecnolandJavaApp {
         boolean bandera = true;
       
         
+        //inicializar el juego
         
+        Juego j1 = new Juego("CSGO");
         
        do{
         Scanner leerprincipio = new Scanner(System.in);    
@@ -35,11 +37,47 @@ public class TecnolandJavaApp {
         switch(nmenu){
             
             case 1:
+                System.out.println("<<<<<<<<<<<<>>>>>>>>>>>>");
+        
+                System.out.println("1 >> Añadir jugador");
+                
+                System.out.println("________________________"); 
+                
+                String addgametag="";
+                int addpuntos=0;
+                
+                System.out.println("Dime tu gamertag");
+                                
+                Scanner addleer1 = new Scanner(System.in);
+                
+                
+                addgametag = addleer1.nextLine();
+                
+               
+                
+                Jugador newj = new Jugador(addgametag, addpuntos);
+                
+                j1.añadirJugador(newj);
+                
+                System.out.println("<<<<<<<<<<<<>>>>>>>>>>>>"); 
                 
                 
                 break;
             
             case 2:
+                System.out.println("<<<<<<<<<<<<>>>>>>>>>>>>");
+        
+                System.out.println("2 >> Borrar jugador");
+                
+                System.out.println("________________________"); 
+                
+                System.out.println("Dime el gamtag que quieres borrar");
+                Scanner leerborrar = new Scanner(System.in);
+                String personaborrar;
+                personaborrar= leerborrar.nextLine();
+                j1.borrarJugador(personaborrar);
+                
+                System.out.println("<<<<<<<<<<<<>>>>>>>>>>>>"); 
                 
                 
                 break;
@@ -55,6 +93,13 @@ public class TecnolandJavaApp {
                 break;
                 
             case 5:
+                
+                 System.out.println("<<<<<<<<<<<<>>>>>>>>>>>>");
+        
+                System.out.println("Saliendo...");  
+                
+                 System.out.println("<<<<<<<<<<<<>>>>>>>>>>>>");
+                bandera=false;
                 
                 break;
             
